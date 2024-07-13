16dadf2ce6dd493a00bcd6fb5a29852c0f9f334a
Lucas Perez <you@example.com>
2024-07-12 23:22:11 -0400

    update logic

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Module15-Leaflet-challenge
 UofT Bootcamp
+For this challenge we use JavaScript and Leaflet to create interactive map that plot eathquake data from USGS GeoJSON API.
+
+#Resources
+I had guidance from Rob Molenda and Munneb Samad.
+- https://github.com/robmolenda/leaflet-challenge/tree/main
+- https://github.com/muneebsamad/leaflet-challenge/blob/main/readme.md
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
 // Initialize the map
-let myMap = L.map("map", {center: [20, 0], zoom: 2});
+let myMap = L.map("map", { center: [20, 0], zoom: 2 });
 
 // Add a tile layer (background map image) to the map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -9,73 +9,64 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Store our API endpoint as queryUrl.
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-//// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(function (data) {
     // Once we get a response, send the data.features object to the createFeatures function
     createFeatures(data.features);
-  });
+});
 
- // Function to determine marker color based on earthquake depth
- function markerColor(depth) {
+// Function to determine marker color based on earthquake depth
+function markerColor(depth) {
     if (depth > 90) return "#CC0000";
     else if (depth > 70) return "#FF3333";
     else if (depth > 50) return "#FF6200";
     else if (depth > 30) return "#00F3FF";
-    else if (depth > 10) return "1BFB00";
+    else if (depth > 10) return "#1BFB00";
     else return "#00CC00";
 }
 
-// Function to create markers
-function createMarkers(feature) {
-    return L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-        radius: markerSize(feature.properties.mag),
-        fillColor: markerColor(feature.geometry.coordinates[2]),
-        color: "#000000",
-        weight: 0.5,
-        opacity: 1,
-        fillOpacity: 0.8
-    }).bindPopup(`<h3>${feature.properties.place}</h3><hr><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]} km</p>`);
-}
-
-  // Function to define radius of earthquake marker
-  function getRadius(magnitude) {
+// Function to define the radius of the earthquake marker
+function getRadius(magnitude) {
     return magnitude * 4;
 }
 
-   // Parse JSON data to create map features
-   L.geoJSON(data, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, {
-            radius: getRadius(feature.properties.mag),
-            fillColor: markerColor(feature.geometry.coordinates[2]),
-            color: "black",
-            weight: 0.5,
-            opacity: 1,
-            fillOpacity: 0.5
-        });
-    },
-    onEachFeature: function (feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
-    }
-}).addTo(myMap);
+// Function to create features and add them to the map
+function createFeatures(earthquakeData) {
+    // Parse JSON data to create map features
+    L.geoJSON(earthquakeData, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: getRadius(feature.properties.mag),
+                fillColor: markerColor(feature.geometry.coordinates[2]),
+                color: "#000000",
+                weight: 1,
+                stroke: true,
+                fillOpacity: 0.5
+            });
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+        }
+    }).addTo(myMap);
 
-   // Create a legend
-   const legend = L.control({ position: "bottomright" });
+    // Create a legend
+    const legend = L.control({ position: "bottomright" });
 
-   legend.onAdd = function() {
-       const div = L.DomUtil.create("div", "legend");
-       const grades = [0, 10, 30, 50, 70, 90];
-       const colors = ["##00CC00", "#1BFB00", "#00F3FF", "#FF6200", "#FF3333", "#CC0000"];
+    legend.onAdd = function () {
+        const div = L.DomUtil.create("div", "legend");
+        const grades = [0, 10, 30, 50, 70, 90];
+        const colors = ["#00CC00", "#1BFB00", "#00F3FF", "#FF6200", "#FF3333", "#CC0000"];
 
-       div.innerHTML = "<h4>Depth (km)</h4>";
+        div.innerHTML = "<h4>Depth (km)</h4>";
 
-       for (let i = 0; i < grades.length; i++) {
-           div.innerHTML +=
-               '<i style="background:' + colors[i] + '"></i> ' +
-               grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-       }
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + colors[i] + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
 
-       return div;
-   };
+        return div;
+    };
 
-   legend.addTo(map);
+    legend.addTo(myMap);
+}
